d13d2f47feac1e54a1cc43b8500e6e9e5b8b9c45
Fabrizio <fabacasa@gmail.com>
2021-10-05 00:14:14 +0200

    debounce update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,6 @@ const Nav = () => {
     <h3>This is the nav</h3>
     <ul>
       <li><Link to="/">Home</Link></li>
-      <li><Link to="/form">Search Pokemons!</Link></li>
       <li><Link to="/pokedex">Pokedex!</Link></li>
     </ul>
   </nav>)
